0f8fea6906278a5f2c60e6f5046ae7e269324a75
Andrea Benfatto <andreabenfatto@gmail.com>
2021-06-22 09:14:37 +0200

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-INPUT_CONFIG_PATH="$1"
+INPUT_CONFIG_PATH="$CONFIG_PATH"
+INPUT_REPORT="$REPORT"
+echo "DEBUGGGGGGGG: $INPUT_REPORT - $INPUT_CONFIG_PATH"
 CONFIG=""
 
 # check if a custom config have been provided
@@ -15,7 +17,7 @@ DONATE_MSG="👋 maintaining gitleaks takes a lot of work so consider sponsoring
 if [ "$GITHUB_EVENT_NAME" = "push" ]
 then
   echo gitleaks --path=$GITHUB_WORKSPACE --verbose --redact $CONFIG
-  CAPTURE_OUTPUT=$(gitleaks --path=$GITHUB_WORKSPACE --verbose --redact $CONFIG)
+  CAPTURE_OUTPUT=$(gitleaks --path=$GITHUB_WORKSPACE --verbose --report=output.json --redact $CONFIG)
 elif [ "$GITHUB_EVENT_NAME" = "pull_request" ]
 then 
   git --git-dir="$GITHUB_WORKSPACE/.git" log --left-right --cherry-pick --pretty=format:"%H" remotes/origin/$GITHUB_BASE_REF... > commit_list.txt
